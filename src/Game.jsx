ebdc8bd4d10617666b73fcac0f61b1257a169318
jaelani27 <abaysptr221@gmail.com>
2025-08-11 00:49:59 +0700

    Create Game.jsx

---
+++ src/Game.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Game() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>🚗 Mobil Sapi 🐄</h1>
+      <img
+        src="/sprites/cow-car.png"
+        alt="Cow Car"
+        width="200"
+      />
+    </div>
+  );
+}
